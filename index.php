9158ad67fc25f4652dfa7a173128df178afa9dc7
Walid <2111708@talnet.nl>
2020-09-02 17:36:06 +0200

    je message

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ print_r($arrayName);
 foreach ($arrayName as $value) {
   echo $value . PHP_EOL;
 }
-*/
+
 // combinatie opdracht
 $bucketlist = array("");
 $hoeveelheid_activiteiten = readline(" Hoeveel activiteiten wilt u toevoegen aan uw bucketlist? ");
@@ -19,7 +19,7 @@ print_r($bucketlist);
 foreach ($bucketlist as  $value) {
   echo $value . PHP_EOL;
 }
-//vraag 6 lukt me niet voor nu v1.0
 
+*/
 
  ?>
